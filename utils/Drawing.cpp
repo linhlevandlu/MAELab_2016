8622b27359bd1f0146d33ac7644216da9e585bde
linh <linh@laptop-147-210-129-159.labri.fr>
2017-01-10 16:02:56 +0100

    add

--- utils/Drawing.cpp
+++ utils/Drawing.cpp
@@ -14,100 +14,113 @@ using namespace std;
 
 #include "Drawing.h"
 
-// draw a line using MidPoint algorithm
-vector<ptr_Point> drawingLine(ptr_Line line, RGB color)
+// draw a line using Breshenham algorithm
+vector<Point> drawingLine(Line line, RGB color)
 {
-	ptr_Point begin = line->getBegin();
-	ptr_Point end = line->getEnd();
-	begin->setColor(color);
-	end->setColor(color);
-	int dx = end->getX() - begin->getX();
-	int dy = end->getY() - begin->getY();
-	vector<ptr_Point> listOfPoints;
-	int x = begin->getX();
-	int y = begin->getY();
-	listOfPoints.push_back(new Point(x, y, color));
-	if (dy < dx)
+	Point begin = line.getBegin();
+	Point end = line.getEnd();
+	begin.setColor(color);
+	end.setColor(color);
+	int dx = abs(end.getX() - begin.getX());
+	int dy = abs(end.getY() - begin.getY());
+
+	int x = begin.getX();
+	int y = begin.getY();
+	int c = dx - dy;
+	int c2 = 2 * c;
+
+	int x_unit = 1, y_unit = 1;
+
+	if (end.getX() - begin.getX() < 0)
+		x_unit = -x_unit;
+	if (end.getY() - begin.getY() < 0)
+		y_unit = -y_unit;
+	vector<Point> listOfPoints;
+	listOfPoints.push_back(Point(x, y, color));
+	// vertical line
+	if (begin.getX() == end.getX())
 	{
-		float p = dy - dx / 2;
-		while (x < end->getX())
+		while (y != end.getY())
 		{
-			if (p >= 0)
-			{
-				listOfPoints.push_back(new Point(x + 1, y + 1, color));
-				y++;
-				p += dy - dx;
-			}
-			else
-			{
-				listOfPoints.push_back(new Point(x + 1, y, color));
-				p += dy;
-			}
-			x++;
+			y += y_unit;
+			listOfPoints.push_back(Point(x, y, color));
 		}
 	}
 	else
-	{
-		float p = dx / 2 - y;
-		while (y <= end->getY())
+	{ // horizontal line
+		if (begin.getY() == end.getY())
 		{
-			if (p >= 0)
+			while (x != end.getX())
 			{
-				listOfPoints.push_back(new Point(x + 1, y + 1, color));
-				x++;
-				p += dx - dy;
+				x += x_unit;
+				listOfPoints.push_back(Point(x, y, color));
 			}
-			else
+		}
+		else
+		{
+			if (begin.getX() != end.getX() && begin.getY() != end.getY())
 			{
-				listOfPoints.push_back(new Point(x, y + 1, color));
-				p += dx;
+				while (x != end.getX())
+				{
+					c2 = 2*c;
+					if(c2 > -dy)
+					{
+						c = c- dy;
+						x +=x_unit;
+					}
+					if(c2 <dx)
+					{
+						c +=dx;
+						y += y_unit;
+					}
+
+					listOfPoints.push_back(Point(x, y, color));
+				}
 			}
-			y++;
 		}
 	}
-	listOfPoints.push_back(new Point(end->getX(), end->getY(), color));
 	return listOfPoints;
 }
 
-vector<ptr_Point> put8pixel(ptr_Point center, ptr_Point p, RGB color)
+vector<Point> put8pixel(Point center, Point p, RGB color)
 {
-	int xc = center->getX();
-	int yc = center->getY();
-	int x = p->getX();
-	int y = p->getY();
-	vector<ptr_Point> eightPoints;
-	eightPoints.push_back(new Point(x + xc, y + yc, color));
-	eightPoints.push_back(new Point(-x + xc, y + yc, color));
-	eightPoints.push_back(new Point(x + xc, -y + yc, color));
-	eightPoints.push_back(new Point(-x + xc, -y + yc, color));
-	eightPoints.push_back(new Point( y + xc, x + yc, color));
-	eightPoints.push_back(new Point(-y + xc, x + yc, color));
-	eightPoints.push_back(new Point(y + xc, -x + yc, color));
-	eightPoints.push_back(new Point(-y + xc, -x + yc, color));
+	int xc = center.getX();
+	int yc = center.getY();
+	int x = p.getX();
+	int y = p.getY();
+	vector<Point> eightPoints;
+	eightPoints.push_back(Point(x + xc, y + yc, color));
+	eightPoints.push_back(Point(-x + xc, y + yc, color));
+	eightPoints.push_back(Point(x + xc, -y + yc, color));
+	eightPoints.push_back(Point(-x + xc, -y + yc, color));
+	eightPoints.push_back(Point(y + xc, x + yc, color));
+	eightPoints.push_back(Point(-y + xc, x + yc, color));
+	eightPoints.push_back(Point(y + xc, -x + yc, color));
+	eightPoints.push_back(Point(-y + xc, -x + yc, color));
 	return eightPoints;
 }
-vector<ptr_Point> drawingCircle(ptr_Point center, int radius, RGB color)
+vector<Point> drawingCircle(Point center, int radius, RGB color)
 {
 	int x = 0;
 	int y = radius;
 	int f = 1 - radius;
-	vector<ptr_Point> result;
-	vector<ptr_Point> drawPoints = put8pixel(center,new Point(x,y),color);
-	result.insert(result.end(),drawPoints.begin(),drawPoints.end());
-	while(x < y)
+	vector<Point> result;
+	vector<Point> drawPoints = put8pixel(center, Point(x, y), color);
+	result.insert(result.end(), drawPoints.begin(), drawPoints.end());
+	while (x < y)
 	{
-		if(f < 0)
+		if (f < 0)
 		{
-			f += 2*x + 3;
+			f += 2 * x + 3;
 		}
 		else
 		{
-			f += 2 * (x-y) + 5;
+			f += 2 * (x - y) + 5;
 			y--;
 		}
 		x++;
-		drawPoints = put8pixel(center,new Point(x,y),color);
-		result.insert(result.end(),drawPoints.begin(),drawPoints.end());
+		drawPoints = put8pixel(center, Point(x, y), color);
+		result.insert(result.end(), drawPoints.begin(), drawPoints.end());
 	}
 	return result;
 }
